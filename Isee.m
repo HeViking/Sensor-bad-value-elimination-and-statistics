db875484eea7e2715c5900f7e558fe4c566bb685
HeViking <40972760+HeViking@users.noreply.github.com>
2020-03-30 22:34:56 +0800

    Add files via upload

--- Isee.m
+++ Isee.m
@@ -1,215 +1,215 @@
 function [average,error] = Isee(x0,c,detail)
-% The equivalent precision measurement result is processed, and the expression of the measurement result is output (mean value ± absolute error)
+%对等精密度测量结果进行处理，输出测量结果的表达式(平均值 ± 绝对误差)
 %[average,error] = Isee(x0,c,detail)
-%-----Input parameters-------
-%x0: measured source data (one-dimensional vector: 1*n vector)
-%c: correction value (a number) (default is 0)
-%detail: Whether to display detailed processing results (one number, default value is 0), =0 not displayed, =1 display, = 2 only shows the final result (suitable for multi-category data analysis)
-%-----Output parameters-------
-%average: average
-%error: absolute error
-%—————Example of use————————
+%—————输入参数———————
+%x0:测量的源数据（一维向量:1*n向量）
+%c:修正值（一个数）（默认值为0）
+%detail：是否显示详细处理结果（一个数,默认值为0），=0不显示，=1显示，=2只显示最终结果（适合多类别数据分析）
+%—————输出参数———————
+%average：平均值
+%error：绝对误差
+%—————使用范例———————
 % 1.>>Isee([205.3,204.94,205.63,205.24,206.65,204.97,205.36,205.16,205.01,204.7,205.56,205.35,205.21,205.19,205.21,205.32]);
 %
-% 2. Create a new variable a, double-click to edit the variable, enter or copy the data, and note that it needs to be a 1*n-dimensional vector (ie, horizontally arranged).
-% >>Isee(a);
-% 3. Advanced application
-% >>Isee([1,2,3], 0.1 , 1); % correction value is 0.1 , showing detailed results
+% 2.新建变量a，双击编辑变量，输入或复制数据，注意需为 1*n 维向量(即打横排列)。
+%   >>Isee(a);
+% 3.高级应用
+%   >>Isee([1,2,3],  0.1 , 1);   %修正值为0.1  ， 显示详细结果
 
 
-% important local variable
-%v : residual error (one-dimensional vector)
-%standard : estimated value of standard deviation
-%indeter : random uncertainty
-%avererror : Arithmetic mean standard deviation estimate
-%rela_error : relative error
-%average0 : Average after rounding
-%error0 : absolute error after rounding
+%重要局部变量
+%v : 剩余误差（一维向量）
+%standard ：标准差的估计值
+%indeter ： 随机不确定度
+%avererror : 算术平均值标准差估计值
+%rela_error ： 相对误差
+%average0 ：  四舍五入后的平均值
+%error0 : 四舍五入后的绝对误差
 
-%length0 : the number of original data
-%length : the number of data after the bad value is removed
-%bad : bad value set
-%bad_n : the number of bad values
-%flag_bad: bad value flag, =1 has bad value, =0 no bad value
-%flag_same : data with the same flag, =length all the same
+%length0 ：原数据个数
+%length ：剔除坏值后的数据个数
+%bad : 坏值集合
+%bad_n ：坏值个数
+%flag_bad :坏值标志，=1有坏值，=0无坏值
+%flag_same :数据相同标志 , =length全部相同
 
-% Grubbs coefficient table
-% 1 2 3 4 5 6 7 8 9 10
-G = [ 0 , 0 , 1.15 , 1.46 , 1.67 , 1.82 , 1.94 , 2.03 , 2.11 , 2.18 , ...
-????2.23 , 2.29 , 2.33 , 2.37 , 2.41 , 2.44 , 2.47 , 2.50 , 2.53 , 2.56 , ...
-????2.58, 2.60, 2.62, 2.64, 2.66];
+%格拉布斯系数表
+%       1     2        3       4        5       6          7        8        9        10
+G = [   0  ,  0   ,  1.15  ,  1.46  , 1.67  ,  1.82   ,  1.94  ,  2.03  ,   2.11  ,  2.18, ...
+    2.23 , 2.29 ,  2.33  ,  2.37  , 2.41  ,  2.44   ,  2.47  ,  2.50  ,   2.53  ,  2.56, ...
+    2.58 , 2.60 ,  2.62  ,  2.64  , 2.66];
 
-%***************** Input parameter processing ***************************** ****
+%*****************输入参数处理*********************************
 if(nargin < 3)
-????Detail = 0;
+    detail = 0;
 end
 if(nargin < 2)
-????c = 0; % has no correction value, the correction value is 0
+    c = 0;    %没有输入修正值时，修正值为0
 end
 if(nargin < 1)
-????Error('[Please enter the first parameter, ie source data]. for details, please refer to help HHL');
+    error('【请输入第一个参数，即源数据】。详情请参考 help HHL');
 end
 
-%****************initialization*********************
-Flag_bad = 1; % bad value flag
-Bad_n = 0; % bad value
-[a,length0] = size(x0); % original data length
-Temp = x0;
-for i=1:length0 %Step 1: Add correction value
-????Temp(i) = temp(i) + c;
+%****************初始化*********************
+flag_bad = 1;          %坏值标志
+bad_n = 0;             %坏值个数
+[a,length0] = size(x0);    %原数据长度
+temp = x0;
+for i=1:length0        %第一步：加上修正值
+    temp(i) = temp(i) + c;
 end
 
-if(length0 <= 2) % The number of pending data is less than 2 and exits directly
-????disp ('The amount of data is too small, the amount of data should be greater than 2')
-????return;
+if(length0 <= 2)     %待处理的数据数小于2直接退出
+    disp('数据量太少，数据量应大于2个')
+    return;
 end
 
-%*************************** Exclude bad values******************* *******************
-while(flag_bad == 1) % can end without bad values
-????z = temp;
-????[a,length] = size(temp); %Processing data length
-????
-????Average = mean(z); %Step 2: Average
-????
-????for i=1:length %Step 3: Find residual error
-????????v(i) = z(i) - average;
-????end
-????
-????%Step 4: Find the standard deviation estimate (Bessel formula)
-????Sum1 = 0;
-????for i=1:length
-????????Sum1 = sum1 + v(i)^2;
-????end
-????Standard = sqrt( sum1 / (length - 1 ) );
-????
-????%Step 5: Calculate random uncertainty and eliminate bad values
-????if(length <= 25) % find random uncertainty
-????????Indeter = G(length) * standard;
-????else
-????????Indeter = 3 * standard;
-????end
-????Flag_bad = 0; % culling bad value initialization, no bad value
-????Temp = [];
-????j = 1;
-????for i=1:length % reject bad values
-????????if(abs(v(i)) > indeter) % bad value
-????????????Bad(bad_n+1) = z(i); % records bad values
-????????????Bad_n = bad_n +1;
-????????????Flag_bad = 1; % bad value flag
-????????else
-????????????Temp(j) = z(i);
-????????????j = j+1;
-????????end
-????end
-????
-????% judges the remaining data
-????[a ,length_temp] = size(temp);
-????if(length_temp <= 2 || length_temp < length0 /2) % The number of data to be processed is less than 3 or less than half of the amount of source data
-????????disp ('too bad value, please re-measure')
-????????return;
-????end
+%***************************剔除坏值**************************************
+while(flag_bad == 1)     %没有坏值时可以结束
+    z = temp;
+    [a,length] = size(temp);      %处理数据长度
+    
+    average = mean(z);    %第二步：求平均值
+    
+    for i=1:length        %第三步：求剩余误差
+        v(i) = z(i) - average;
+    end
+    
+    %第四步：求标准差估计值(贝塞尔公式)
+    sum1 = 0;
+    for i=1:length
+        sum1 = sum1 + v(i)^2;
+    end
+    standard = sqrt(  sum1 / (length - 1 )  );
+    
+    %第五步：计算随机不确定度，剔除坏值
+    if(length <= 25)     %求随机不确定度
+        indeter = G(length) * standard;
+    else
+        indeter = 3 * standard;
+    end
+    flag_bad = 0;  %剔出坏值初始化，无坏值
+    temp = [];
+    j = 1;
+    for i=1:length       %剔除坏值
+        if(abs(v(i)) > indeter)   %坏值
+            bad(bad_n+1) = z(i);  %记录坏值
+            bad_n = bad_n +1;
+            flag_bad = 1;         %坏值标志
+        else
+            temp(j) = z(i);
+            j = j+1;
+        end
+    end
+    
+    %判断剩下的数据数
+    [a ,length_temp] = size(temp);
+    if(length_temp <= 2  ||  length_temp < length0 /2)     %待处理的数据数小于3  或 小于源数据量的一半 直接退出
+        disp('坏值太多，请重新测量')
+        return;
+    end
 end
 
-%***************Special case judgment, judge whether the data are equal************************* *****
-Flag_same = 1;
+%***************特殊情况判断，判断数据是否都相等******************************
+flag_same = 1;
 for i=2:length
-????if( abs(z(i) - z(1)) < 10^(-7) )
-????????Flag_same = flag_same + 1;
-????end
+    if(  abs(z(i) - z(1)) <  10^(-7) )
+        flag_same = flag_same + 1;
+    end
 end
-if(flag_same == length) % data is the same, direct output
-????fprintf ('measured data average is %g, absolute error is 0, relative error is 0%%\n', average);
-????fprintf('source data has %d, ', length0);
-????if(bad_n == 0)
-????????fprintf ('good data, no bad value\n');
-????else
-????????fprintf ('bad values ??total %d, as shown below: ', bad_n);
-????????Bad
-????end
-????return;
+if(flag_same == length)  %数据都相同，直接输出
+    fprintf('测量数据平均值为 %g  ,  绝对误差为 0 ， 相对误差为 0 %%\n',average);
+    fprintf('源数据共有%d个，',length0);
+    if(bad_n == 0)
+        fprintf('数据良好，没有坏值\n');
+    else
+        fprintf('坏值共有%d个，如下所示：',bad_n);
+        bad
+    end
+    return;
 end
-%**********************Step 7: Malikov Criterion***************** *********
-Deta = 0;
+%**********************第七步：马利科夫判据**************************
+deta = 0;
 if(detail ~= 2)
-????if(mod(length , 2) == 0) % The length of the data to be processed is even
-????????for i=1:length /2
-????????????Deta = deta + v(i);
-????????end
-????????for i= length /2 +1 : length
-????????????Deta = deta - v(i);
-????????end
-????else % is odd
-????????for i=1:(length-1) /2
-????????????Deta = deta + v(i);
-????????end
-????????for i= (length +3) / 2 : length
-????????????Deta = deta - v(i);
-????????end
-????end
-????
-????if( abs(deta) > abs( max(v) ))
-????????disp ('There is a linearity difference, the data cannot be used in principle!')
-????end
-????
-????%********************Ah - Hemet's Criterion ********************* *********
-????Sum1 = 0;
-????for i=1:length-1
-????????Sum1 = sum1 + v(i)*v(i+1);
-????end
-????
-????if(abs(sum1) > sqrt(length-1)* standard^2)
-????????disp ('There is a periodic difference, the data cannot be used in principle!')
-????end
+    if(mod(length , 2) == 0)  %待处理数据长度为偶数
+        for i=1:length /2
+            deta = deta + v(i);
+        end
+        for i= length /2 +1 : length
+            deta = deta - v(i);
+        end
+    else   %为奇数
+        for i=1:(length-1) /2
+            deta = deta + v(i);
+        end
+        for i= (length +3) / 2 : length
+            deta = deta - v(i);
+        end
+    end
+    
+    if( abs(deta) > abs( max(v) ))
+        disp('存在线性系差，数据原则上不能使用！')
+    end
+    
+    %*******************阿卑——赫梅特判据******************************
+    sum1 = 0;
+    for i=1:length-1
+        sum1 = sum1 + v(i)*v(i+1);
+    end
+    
+    if(abs(sum1)  >  sqrt(length-1)* standard^2)
+        disp('存在周期性系差，数据原则上不能使用！')
+    end
 end
-%**************** Calculate absolute error, relative error **********************
-Avererror = standard / sqrt(length); %Step 8: Calculate the arithmetic mean standard deviation estimate
-if( length <=25 ) % Step 9: Calculate the uncertainty of the arithmetic mean
-????Error = G(length) * avererror;
+%****************计算绝对误差、相对误差********************
+avererror = standard / sqrt(length);    %第八步：计算算术平均值标准差估计值
+if( length <=25 )                       %第九步：计算算术平均值的不确定度
+    error =  G(length) * avererror;
 else
-????Error = 3 * avererror;
+    error =  3 * avererror;
 end
-Rela_error = error/average * 100;
+rela_error = error/average * 100;
 
-%************************Measurement report********************** ******
-% absolute error retains one significant digit and the average remains to the same bit as the absolute error
-Temp = error;
-if (error <1) % absolute error is less than 1
-????for i=1:500
-????????Temp = temp * 10;
-????????if(temp >= 1)
-????????????Break;
-????????end
-????end
-????Wei = i; the number of digits % absolute error is retained
-????Error0 = roundn(error,-wei); % rounded off, reserved to the wei position after the decimal point
-????Average0 = roundn(average,-wei);
+%************************测量结果报告****************************
+%绝对误差保留一位有效数字，平均值保留到与绝对误差相同的位
+temp = error;
+if (error <1)    %绝对误差小于1
+    for i=1:500
+        temp = temp * 10;
+        if(temp >= 1)
+            break;
+        end
+    end
+    wei = i;   %绝对误差保留到的位数
+    error0 = roundn(error,-wei);   %四舍五入，保留到小数点后第wei位
+    average0 = roundn(average,-wei);
 else
-????for i=0:500
-????????Temp = temp / 10;
-????????if(temp <= 1)
-????????????Break;
-????????end
-????end
-????Wei = i; the number of digits % absolute error is retained
-????Error0 = roundn(error,wei); % rounded off, reserved to the first wei+1 before the decimal point
-????Average0 = roundn(average,wei);
+    for i=0:500
+        temp = temp / 10;
+        if(temp <= 1)
+            break;
+        end
+    end
+    wei = i;   %绝对误差保留到的位数
+    error0 = roundn(error,wei);      %四舍五入，保留到小数点前第wei+1位
+    average0 = roundn(average,wei);
 end
 
 if(detail ~= 2)
-????fprintf('final result: %g ± %g\n' , average0 , error0);
+    fprintf('最终结果 ： %g ± %g\n' , average0 , error0);
 else
-????fprintf(' %g ± %g\n' , average0 , error0);
+    fprintf(' %g ± %g\n' , average0 , error0);
 end
 
-if(detail ~= 1) % does not show detailed results
-???? return;
+if(detail ~= 1)   %不显示详细结果
+    return;
 end
 
-fprintf('measured data average is %g, absolute error is %g, relative error is %g %%\n', average, error, rela_error);
-fprintf('source data has %d, ', length0);
+fprintf('测量数据平均值为 %g  ,  绝对误差为 %g ， 相对误差为 %g %%\n',average , error  ,  rela_error);
+fprintf('源数据共有%d个，',length0);
 if(bad_n == 0)
-???? fprintf ('good data, no bad value\n');
+    fprintf('数据良好，没有坏值\n');
 else
-???? fprintf ('bad values total %d, as shown below: ', bad_n);
-???? Bad
+    fprintf('坏值共有%d个，如下所示：',bad_n);
+    bad
 end
--- txtsee.m
+++ txtsee.m
@@ -1,11 +1,12 @@
 function [ ] = txtsee( file )
-% Processes the txt data in the specified format, and outputs the processed result.
+%对既定格式的txt数据进行处理，输出处理后的结果
 %[ ] = txtsee( file )
-% Enter the parameter file: the path to Notepad, such as 'data.txt' (the default directory is matlab's work path) or 'C:\Users\Administrator\Desktop\data.txt'
-% output: average and absolute error of each data
-%Note: This function needs to be used in Matlab2010 or higher.
+%输入参数file：记事本的路径，如'data.txt'(默认目录为matlab的work路径)或'C:\Users\Administrator\Desktop\data.txt'
+%输出结果：每项数据的平均值和绝对误差
+%注意：本函数需在Matlab2010或更高版本使用
 
-class = ['North latitude: '; 'East: '; 'Temperature: '; 'Height: '; 
+%初始化
+class = ['北纬：';'东经：';'温度：';'高度：';'气压：'];
 weidu_n = 1;
 jingdu_n = 1;
 temper_n = 1;
@@ -13,47 +14,47 @@ height_n = 1;
 pressure_n = 1;
 PM_n = 1;
 
-%Open the established file
+%打开既定文件
 fp = fopen(file);
 
 while ~feof(fp)
-    line = fgetl(fp); % reads a line of string data
-    if(strfind(line,class(1,:)) == 1)   % reads a line of string data
-        k = strfind(line , 'degree');  % North latitude
+    line = fgetl(fp); %读取一行字符串数据
+    if(strfind(line,class(1,:)) == 1)   %北纬
+        k = strfind(line , '度');
         temp = line(4:k-1);
         weidu(weidu_n) = str2num(temp);
         weidu_n = weidu_n + 1;
     end
     
-    if(strfind(line,class(2,:)) == 1)   %East
-        k = strfind(line , 'degree');
+    if(strfind(line,class(2,:)) == 1)   %东经
+        k = strfind(line , '度');
         temp = line(4:k-1);
         jingdu(jingdu_n) = str2num(temp);
         jingdu_n = jingdu_n + 1;
     end
     
-    if(strfind(line,class(3,:)) == 1)   %degree Celsius
-        k = strfind(line , 'degree Celsius');
+    if(strfind(line,class(3,:)) == 1)   %温度
+        k = strfind(line , '摄氏度');
         temp = line(4:k-1);
         temper(temper_n) = str2num(temp);
         temper_n = temper_n + 1;
     end
     
-    if(strfind(line,class(4,:)) == 1)   %degree Celsius
-        k = strfind(line , 'm');
+    if(strfind(line,class(4,:)) == 1)   %高度
+        k = strfind(line , '米');
         temp = line(4:k-1);
         height(height_n) = str2num(temp);
         height_n = height_n + 1;
     end
     
-    if(strfind(line,class(5,:)) == 1)   %pressure
+    if(strfind(line,class(5,:)) == 1)   %气压
         k = strfind(line , 'PA');
         temp = line(4:k-1);
         pressure(pressure_n) = str2num(temp);
         pressure_n = pressure_n + 1;
     end
     
-    if(strfind(line,'PM2.5：') == 1)  %PM2.5, the length is different
+    if(strfind(line,'PM2.5：') == 1)  %PM2.5，长度不一样
         k = strfind(line , 'ug/m3');
         temp = line(7:k-1);
         PM(PM_n) = str2num(temp);
@@ -63,8 +64,8 @@ end
 fclose(fp);
 
 fprintf('%s',class(1,:));
-Isee(weidu,0,2);  % Processes the acquired data. See help Isee for details.
-??????????????????% To view specific information, change the third parameter from 2 to 1.
+Isee(weidu,0,2);  %对获取到的数据进行处理，详情参考help Isee
+                  %若要查看具体信息，可将第三个参数从2改为1
 fprintf('%s',class(2,:));
 Isee(jingdu,0,2);
 
